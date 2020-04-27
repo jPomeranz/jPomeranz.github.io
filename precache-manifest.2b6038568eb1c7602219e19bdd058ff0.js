f83f897d9bef0995c9df53db814c4c53226cfee8
Jacob Pomeranz <jacobp@qualtrics.com>
2020-04-26 17:24:18 -0700

    Updates

---
+++ precache-manifest.2b6038568eb1c7602219e19bdd058ff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9a935919cc91ebc4c1d7e4e011f14c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc893fda965cbeace2fa",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "ae3f6cb89e39b987d88d",
+    "url": "/static/css/main.42d5acbd.chunk.css"
+  },
+  {
+    "revision": "bc893fda965cbeace2fa",
+    "url": "/static/js/2.1a57e062.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a57e062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3f6cb89e39b987d88d",
+    "url": "/static/js/main.abb1f170.chunk.js"
+  },
+  {
+    "revision": "21cc19aaa8f5035a8745",
+    "url": "/static/js/runtime-main.44dd3b04.js"
+  }
+]);
--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-function Footer() {
-  const date = new Date();
-  return <div className="footer">{`© Copyright ${date.getFullYear()}.`}</div>;
-}
-
-export default Footer;
